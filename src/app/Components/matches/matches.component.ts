fb0d6c59fe6c37dac631f887c726f647fb6ad436
Andrei <andrei.massaini@hotmail.com>
2024-03-06 06:43:41 -0300

    trying to add download matches functionality

--- src/app/Components/matches/matches.component.ts
+++ src/app/Components/matches/matches.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { folderContentResponse } from '../../Response/Folder/folderContentResponse';
+import { HttpClient } from '@angular/common/http';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-matches',
@@ -11,4 +13,21 @@ import { folderContentResponse } from '../../Response/Folder/folderContentRespon
 export class MatchesComponent {
 
   @Input() matchesArrayReciever: folderContentResponse[] = [];
+
+  constructor(private http: HttpClient) {}
+  
+  downloadMatches() {
+    for (let matches of this.matchesArrayReciever) {
+      this.downloadFile(matches.URL, matches.fileName);
+    }
+  }
+
+  downloadFile(url: string, fileName: string) {
+    console.log('Baixando arquivo:', url);
+    this.http.get(url, { responseType: 'blob' }).subscribe(response => {
+      saveAs(response, fileName); // Inicia o download do arquivo
+    }, error => {
+      console.error('Erro ao baixar o arquivo:', error);
+    });
+  }
 }
